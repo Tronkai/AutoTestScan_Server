362afdbe92105593760a9c970b5f3660303e1219
Tronkai <kaizhu@tron.network>
2020-04-28 15:42:14 +0800

    add cros

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ var tronlinUIRouter = require('./routes/tronlinkUI')
 var usersRouter = require('./routes/users')
 var app = express();
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, mytoken')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization')
+  res.setHeader('Content-Type', 'application/json;charset=utf-8')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('X-Powered-By', ' 3.2.1')
   next();
 });
 app.use("/",tronscanUIRouter);
